3f7c847b100939334abde6dc59faea794d2bf2e6
Bruno Fialho <brugerf@gmail.com>
2022-08-09 20:42:08 -0300

    feat: marketplace implementation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeContract } from "./utils/near";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,16 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+window.nearInitPromise = initializeContract()
+  .then(() => {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  })
+  .catch(console.error);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
